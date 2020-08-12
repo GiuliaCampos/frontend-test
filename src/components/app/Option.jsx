a5fefe7205da3bb3521efbee9145068d3f192345
Giulia Campos <38157795+GiuliaCampos@users.noreply.github.com>
2020-08-12 17:18:08 -0300

    Componente com opções do dropdown

---
+++ src/components/app/Option.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default styled.option`
+width: 300px;
+height: 40px;
+border-color: #fff;
+border-radius: 50px;
+margin: 20px;
+padding: 20px;
+box-shadow: 0px 5px 6px -3px #000;
+`
