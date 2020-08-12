82818bc0e9d67895896b349da7fb8e1350733c29
Giulia Campos <38157795+GiuliaCampos@users.noreply.github.com>
2020-08-12 17:17:49 -0300

    Componente de input

---
+++ src/components/app/Input.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export default styled.input `
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    width: 300px;
+    height: 40px;
+    border-color: #fff;
+    border-radius: 50px;
+    margin: 20px;
+    padding: 20px;
+    box-shadow: 0px 5px 6px -3px #000;
+
+    &.numeroPopulacao{
+    width: 180px;
+    }
+`
