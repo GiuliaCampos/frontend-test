52f3882279b8cdd9f798922856fc4420090b91e1
Giulia Campos <38157795+GiuliaCampos@users.noreply.github.com>
2020-08-12 17:17:20 -0300

    Componente Dropdown

---
+++ src/components/app/Dropdown.jsx
@@ -0,0 +1,14 @@
+
+import styled from 'styled-components';
+
+export default styled.select`
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  width: 300px;
+  height: 40px;
+  border-color: #fff;
+  border-radius: 50px;
+  margin: 20px;
+  box-shadow: 0px 5px 6px -3px #000;
+`
