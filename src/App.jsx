c90f406d4b9a1230236f6d0282960a80f6843758
Giulia Campos <38157795+GiuliaCampos@users.noreply.github.com>
2020-08-13 15:25:06 -0300

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import {HashRouter, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
+import {HashRouter, BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import Home from './components/app/Pages/index'
 import Login from './components/app/Pages/login'
 
-function App(){
+import { isAuthenticated } from './services/auth'
+
+const PrivateRoute =({ component: Component, ...rest}) => (
+    <Route 
+        {...rest}
+        render={props => isAuthenticated() ? (
+            <Component {...props} />
+        ) : (
+            <Redirect to={{pathname: "/", state: {from: props.location}}} />
+        )}
+    />
+)
+
+const App = () => {
     return(
         <HashRouter>
             <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/login" component={Login} />
+                <Route exact path="/" component={Login} />
+                <PrivateRoute exact path="/home" component={Home} />
             </Switch>
         </HashRouter>
     )
--- src/components/app/Pages/login.jsx
+++ src/components/app/Pages/login.jsx
@@ -1,17 +1,39 @@
-import 'regenerator-runtime/runtime'
-import React from 'react';
+import React, {useState} from 'react';
+import { Link, withRouter } from "react-router-dom";
+
+import {login} from '../../../services/auth'
 
 import Theme from '../../theme';
 import Layout from '../../layout';
 
 import H1 from '../H1';
+import Input from '../Input'
+import Button from  '../Button'
 
 
-function Login(){
+function Login(props){
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  function handleSignin(){
+    if(!email || !password){
+      setError("Preencha todos os campos")
+    } else{
+      const response = Math.random();
+      login(response)
+      props.history.push("/home")
+    }
+  }
+
   return(
     <Theme>
       <Layout>
         <H1>Login</H1>
+        {error && <p>{error}</p>}
+        <Input value={email} placeholder="E-mail" required type="email" onChange={e => setEmail(e.target.value)}/>
+        <Input value={password} placeholder="Password" required type="password" onChange={e => setPassword(e.target.value)} />
+        <Button label="login" onClick={handleSignin} />
       </Layout>
     </Theme>
   )
